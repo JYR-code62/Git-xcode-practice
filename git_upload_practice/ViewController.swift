0c1739e78793e484508a065796e1ec030a796ced
JYR-code62 <engineermemu9@gmail.com>
2021-08-03 09:51:16 +0530

    First Initial Commit -practice project Xcode

---
+++ git_upload_practice/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  git_upload_practice
+//
+//  Created by Jyoti1 Yadav on 03/08/21.
+//  Copyright © 2021 Jyoti1 Yadav. All rights reserved.
+//
+
+import UIKit
+
+// practice 
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+
+}
+
